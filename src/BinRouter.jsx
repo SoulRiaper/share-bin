fc83bc56d8dafb541873156e81dbbb2d8d5eb347
SoulRiaper <soulriaper@gmail.com>
2025-03-24 16:50:41 +0300

    update router - handle app hash and draw content

--- src/BinRouter.jsx
+++ src/BinRouter.jsx
@@ -1,11 +1,42 @@
+import React, { useEffect, useState } from "react";
 import BinEditor from "./BinEditor";
 import BinForm from "./BinForm";
 import Form from "./common/Form";
 import TagEditor from "./TagEditor";
+import parsePath from "./utils/pathParser";
 
-function BinRouter () {
+function BinRouter ({children, defaultComponent}) {
+  const [child, setChild] = useState(null)
+
+  const handleHash = () => {
+    const hash = location.hash.slice(1);
+    let validChild = (defaultComponent);
+    React.Children.forEach(children, (ch) => {
+      const { pattern } = ch.props;
+      const additionalProps = parsePath(hash, pattern)
+      if (additionalProps != null) {
+        validChild = React.cloneElement(ch, {...additionalProps})
+      }
+    })
+    if (validChild) {
+      setChild(validChild);
+    }
+  }
+
+  const windowListener = () => { handleHash() };
+
+  useEffect(() => {
+    window.addEventListener('hashchange', windowListener);
+    windowListener();
+    return () => {
+      window.removeEventListener('hashchange', windowListener)
+    }
+  }, []);
+
   return (
-    <BinForm></BinForm>
+    <>
+      {child}
+    </>
   )
 }
 
